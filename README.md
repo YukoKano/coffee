e641bca0271f6ff8ab61fbb9d074d10e984cd629
YukoKano <halloween_1031@hotmail.co.jp>
2024-02-02 00:23:22 +0900

    emotionを入れ直しました

--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-- 学ぶ技術
-  - React Hooks詳細
-  - js配列操作
-  - lint
-  - storybook(触りだけ)
-
----
-
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 ## Getting Started
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,29 @@
+import Head from "next/head";
+import Image from "next/image";
 import { css } from "@emotion/react";
 
-const style = css`
-  color: red;
-  margin: 8px;
+const image = css`
+  width: 100%;
+  height: auto;
 `;
 
 export default function Home() {
-  return <p css={style}>hoge</p>;
+  return (
+    <>
+      <Head>
+        <title>Coffee Timer App</title>
+        <meta name="description" content="Coffee Timer App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        css={image}
+        src="/coffee-barista.png"
+        width={500}
+        height={500}
+        alt="hoge"
+        priority
+      />
+    </>
+  );
 }

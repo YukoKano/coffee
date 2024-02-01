95f727cee53afd02a6daa2f4a50c4d2fcabb0657
YukoKano <halloween_1031@hotmail.co.jp>
2024-02-02 00:40:14 +0900

    モード変更をuseStateで実装

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
+import Head from "next/head";
 import "@/styles/reset.css";
+import "@/styles/global.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Coffee Timer App</title>
+        <meta name="description" content="Coffee Timer App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }

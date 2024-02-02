6781bf8f5329f54a2017c766b192ddaf1a2e1572
YukoKano <halloween_1031@hotmail.co.jp>
2024-02-03 01:10:08 +0900

    Revert "モード変更をuseStateで実装"
    This reverts commit 95f727cee53afd02a6daa2f4a50c4d2fcabb0657.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
+import Head from "next/head";
 import Image from "next/image";
 import { css } from "@emotion/react";
-
-const nightMode = css`
-  background-color: #638889;
-`;
-
-const lightMode = css`
-  background-color: #f9efdb;
-`;
 
 const image = css`
   width: 100%;
   height: auto;
 `;
 
-const ToggleModeButton = ({ mode, setMode }) => {
-  const handleClick = (e) => {
-    e.preventDefault();
-    setMode(!mode);
-  };
-  return <button onClick={handleClick}>toggle mode</button>;
-};
-
 export default function Home() {
-  const [mode, setMode] = useState(false);
-  const modeStyle = mode ? nightMode : lightMode;
-
   return (
-    <main css={modeStyle}>
+    <>
+      <Head>
+        <title>Coffee Timer App</title>
+        <meta name="description" content="Coffee Timer App" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Image
         css={image}
         src="/coffee-barista.png"
@@ -37,7 +24,6 @@ export default function Home() {
         alt="hoge"
         priority
       />
-      <ToggleModeButton mode={mode} setMode={setMode} />
-    </main>
+    </>
   );
 }

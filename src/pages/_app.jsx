3b147e5d7a454c513e252b0a776d250999e72ddc
YukoKano <halloween_1031@hotmail.co.jp>
2024-02-03 17:42:08 +0900

    指定できることは確認したので元に戻す

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -26,12 +26,12 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content="Coffee Timer App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* ↓使ってモード切り替え簡単にできないかな →メディアクエリで行けそう */}
-        <meta name="theme-color" content="#472712" />
-        {/* <meta
+        <meta name="theme-color" content="#fff4e5" />
+        <meta
           name="theme-color"
           media={`prefers-color-scheme: ${mode ? "light" : "dark"}`}
           content={mode ? "#fff4e5" : "#472712"}
-        /> */}
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Providerのvalueは{()}ではなく、{{}} */}

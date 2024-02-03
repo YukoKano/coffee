119a69c6a8eab9584de42ccb87e3eaa5e1dd858a
YukoKano <halloween_1031@hotmail.co.jp>
2024-02-03 17:25:15 +0900

    画面固定したいので修正

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,14 +7,12 @@ import { useState } from "react";
 import { modeContext } from "../components/modeProvider";
 
 const nightMode = css`
-  overflow: hidden;
   min-height: 100vh;
   background-color: #472712;
 `;
 
 const lightMode = css`
   min-height: 100vh;
-  overflow: hidden;
   background-color: #fff4e5;
 `;
 
@@ -27,7 +25,8 @@ export default function App({ Component, pageProps }) {
         <title>Coffee Timer App</title>
         <meta name="description" content="Coffee Timer App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* ↓使ってモード切り替え簡単にできないかな */}
+        {/* ↓使ってモード切り替え簡単にできないかな →メディアクエリで行けそう */}
+        <meta name="theme-color" content="#fff4e5" />
         <meta
           name="theme-color"
           media={`prefers-color-scheme: ${mode ? "light" : "dark"}`}

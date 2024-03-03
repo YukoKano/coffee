bce4041b07bbadbb6bb356427402c6e209cb5e23
YukoKano <halloween_1031@hotmail.co.jp>
2024-03-03 17:27:02 +0900

    コメントつける

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -1,14 +1,20 @@
 import axios from "axios";
 
-// ↓わかんない！！
-export default async function handler(req, res) {
+// async = 非同期しますよ！
+async function handler(req, res) {
+  // 処理をするよ！エラーがあったら受け取るよ！
   try {
+    // await = この処理が返ってくるまで待ってね！
     const response = await axios.get(
       "https://coffee.alexflipnote.dev/random.json"
     );
+    // 返ってきた処理をjsonで返すよ！
     res.status(200).json(response.data);
   } catch (error) {
     console.error("Error fetching data:", error);
+    // Networkできちんと表示するよ！
     res.status(500).json({ error: "Error fetching data" });
   }
 }
+
+export default handler;

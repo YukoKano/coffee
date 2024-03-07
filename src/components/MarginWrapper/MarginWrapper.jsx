554f25ea20914e82f1dcc2cbb799b47cc55bdc3d
YukoKano <halloween_1031@hotmail.co.jp>
2024-03-07 09:29:24 +0900

    CSSの関数の引数の書き方変更

--- src/components/MarginWrapper/MarginWrapper.jsx
+++ src/components/MarginWrapper/MarginWrapper.jsx
@@ -1,10 +1,10 @@
 import { css } from "@emotion/react";
 
-const margin = ({ value }) => css`
+const margin = (value) => css`
   margin-top: ${value * 8}px;
 `;
 
 export const MarginWrapper = ({ children, value }) => {
-  return <div css={margin({ value })}>{children}</div>;
+  return <div css={margin(value)}>{children}</div>;
   // valueは{{}}で渡すっぽい
 };
